93cbb568c842cce8b77c55b7043bf0472c8d68b7
yahortsaryk <egortsaryk@gmail.com>
2025-03-28 13:18:44 +0100

    fix: using 'sp_std::vec' dependency

--- src/api.rs
+++ src/api.rs
@@ -7,7 +7,7 @@ use ddc_primitives::{
     VERIFY_AGGREGATOR_RESPONSE_SIGNATURE,
 };
 use proto::{endpoint_itm_table::Variant as ItmTableVariant, ItmTable};
-use scale_info::prelude::vec;
+use sp_std::vec;
 use scale_info::{
     prelude::{format, string::String},
     TypeInfo,
