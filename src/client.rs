c076f3b56514e2a10ef07eb718089a96d674cdb7
yahortsaryk <egortsaryk@gmail.com>
2025-06-12 12:51:00 +0200

    chore: details to api errors added; tracing logs; new inspection exceptions types; improved discovery of G-Collectors and Sync Nodes in DDC

--- src/client.rs
+++ src/client.rs
@@ -10,7 +10,7 @@ use sp_runtime::offchain::{http, Duration};
 use sp_std::vec;
 
 use super::*;
-use crate::{json, verification::Verify};
+use crate::{json, log, verification::Verify};
 
 pub struct DdcClient<'a> {
     pub base_url: &'a str,
@@ -44,12 +44,13 @@ macro_rules! fetch_and_parse_json {
         if $self.verify_sig {
             let json_signed_response: json::SignedJsonResponse<$signed_ty> =
                 serde_json::from_slice(&body).map_err(|e| {
-                    log::error!("❌ Failed to parse signed .json: {:?}", e);
+                    log!(error, "❌ Failed to parse signed .json: {:?}", e);
                     http::Error::Unknown
                 })?;
 
             if !json_signed_response.verify() {
-                log::error!(
+                log!(
+                    error,
                     "❌ Bad .json signature, req: {:?}, resp: {:?}",
                     $url,
                     json_signed_response
@@ -65,11 +66,10 @@ macro_rules! fetch_and_parse_json {
 
             Ok((json_response, Some(signed_by)))
         } else {
-            let json_response: $unsigned_ty =
-                serde_json::from_slice(&body).map_err(|e| {
-                    log::error!("❌ Failed to parse unsigned .json: {:?}", e);
-                    http::Error::Unknown
-                })?;
+            let json_response: $unsigned_ty = serde_json::from_slice(&body).map_err(|e| {
+                log!(error, "❌ Failed to parse unsigned .json: {:?}", e);
+                http::Error::Unknown
+            })?;
 
             Ok((json_response, None))
         }
@@ -103,7 +103,8 @@ macro_rules! fetch_and_parse_proto {
                 .map_err(|_| http::Error::Unknown)?;
 
             if !proto_signed_response.verify() {
-                log::error!(
+                log!(
+                    error,
                     "❌ Bad .proto signature, req: {:?}, resp: {:?}",
                     $url,
                     proto_signed_response
@@ -113,10 +114,11 @@ macro_rules! fetch_and_parse_proto {
 
             let proto_response: $signed_ty =
                 <$signed_ty>::decode(proto_signed_response.payload.as_slice())
-                    .map_err(|_| http::Error::Unknown).map_err(|e| {
-                    log::error!("❌ Failed to parse signed .proto: {:?}", e);
-                    http::Error::Unknown
-                })?;
+                    .map_err(|_| http::Error::Unknown)
+                    .map_err(|e| {
+                        log::error!("❌ Failed to parse signed .proto: {:?}", e);
+                        http::Error::Unknown
+                    })?;
             let signed_by = proto_signed_response
                 .signature
                 .map(|v| SignedBy {
@@ -127,9 +129,10 @@ macro_rules! fetch_and_parse_proto {
 
             Ok((proto_response, Some(signed_by)))
         } else {
-            let proto_response: $unsigned_ty =
-                <$unsigned_ty>::decode(body.as_slice()).map_err(|_| http::Error::Unknown).map_err(|e| {
-                    log::error!("❌ Failed to parse unsigned .proto: {:?}", e);
+            let proto_response: $unsigned_ty = <$unsigned_ty>::decode(body.as_slice())
+                .map_err(|_| http::Error::Unknown)
+                .map_err(|e| {
+                    log!(error, "❌ Failed to parse unsigned .proto: {:?}", e);
                     http::Error::Unknown
                 })?;
 
@@ -262,7 +265,11 @@ impl<'a> DdcClient<'a> {
         Ok(api_response)
     }
 
-    pub fn eras(&self, prev: Option<EhdEra>, limit: Option<u32>) -> Result<ApiResponse<Vec<json::AggregationEraResponse>>, http::Error> {
+    pub fn eras(
+        &self,
+        prev: Option<EhdEra>,
+        limit: Option<u32>,
+    ) -> Result<ApiResponse<Vec<json::AggregationEraResponse>>, http::Error> {
         let mut url = format!("{}/activity/eras", self.base_url);
         if let Some(prev) = prev {
             url = format!("{}?prevToken={}", url, prev);
@@ -286,11 +293,15 @@ impl<'a> DdcClient<'a> {
             response,
             signed_by,
         };
-        
+
         Ok(api_response)
     }
 
-    pub fn payment_eras(&self, prev: Option<EhdEra>, limit: Option<u32>) -> Result<ApiResponse<Vec<json::EHDEra>>, http::Error> {
+    pub fn payment_eras(
+        &self,
+        prev: Option<EhdEra>,
+        limit: Option<u32>,
+    ) -> Result<ApiResponse<Vec<json::EHDEra>>, http::Error> {
         let mut url = format!("{}/activity/payment-eras", self.base_url);
         if let Some(prev) = prev {
             url = format!("{}?prevToken={}", url, prev);
@@ -302,8 +313,9 @@ impl<'a> DdcClient<'a> {
                 url = format!("{}?limit={}", url, limit);
             }
         }
-        
-        let (response, signed_by) = fetch_and_parse_json!(self, url, Vec<json::EHDEra>, Vec<json::EHDEra>)?;
+
+        let (response, signed_by) =
+            fetch_and_parse_json!(self, url, Vec<json::EHDEra>, Vec<json::EHDEra>)?;
 
         let api_response = ApiResponse {
             response,
@@ -439,7 +451,7 @@ impl<'a> DdcClient<'a> {
         let body = response.body().collect::<Vec<u8>>();
         let proto_response = proto::inspection::EndpointItmGetPathsState::decode(body.as_slice())
             .map_err(|e| {
-            log::info!("Decode ITM Path Report protobuf error: {:?}", e);
+            log!(error, "❌ Decode ITM Path Report protobuf error: {:?}", e);
             http::Error::Unknown
         })?;
 
@@ -490,6 +502,18 @@ impl<'a> DdcClient<'a> {
             url,
             json::IsGCollectorResponse,
             json::IsGCollectorResponse
+        )?;
+
+        Ok(response)
+    }
+
+    pub fn get_grouping_collectors(&self) -> Result<json::GCollectorsResponse, http::Error> {
+        let mut url = format!("{}/activity/grouping-collectors", self.base_url);
+        let (response, _) = fetch_and_parse_json!(
+            self,
+            url,
+            json::GCollectorsResponse,
+            json::GCollectorsResponse
         )?;
 
         Ok(response)
@@ -557,7 +581,13 @@ impl<'a> DdcClient<'a> {
         let deadline = timestamp().add(self.timeout);
         let mut error = None;
 
-        for _ in 0..self.retries {
+        for i in 0..self.retries {
+            log!(
+                trace,
+                "Sending HTTP GET request to {:?}, attempt: {:?}",
+                url,
+                i + 1
+            );
             let mut request = http::Request::get(url).deadline(deadline);
             request = match accept {
                 Accept::Any => request,
@@ -586,17 +616,38 @@ impl<'a> DdcClient<'a> {
         }
 
         if let Some(e) = error {
+            log!(
+                error,
+                "❌ HTTP GET client error for url {:?}, error {:?}",
+                url,
+                e
+            );
             return Err(e);
         }
 
         let response = match maybe_response {
             Some(r) => r,
-            None => return Err(http::Error::Unknown),
+            None => {
+                log!(
+                    error,
+                    "❌ HTTP GET client error for url {:?}, no response",
+                    url
+                );
+                return Err(http::Error::Unknown);
+            }
         };
 
         if response.code >= 500 {
+            log!(
+                error,
+                "❌ HTTP GET client error for url {:?}, status code is {:?}",
+                url,
+                response.code
+            );
             return Err(http::Error::Unknown);
         }
+
+        log!(trace, "HTTP GET request to {:?} completed", url,);
 
         Ok(response)
     }
@@ -612,7 +663,13 @@ impl<'a> DdcClient<'a> {
         let deadline = timestamp().add(self.timeout);
         let mut error = None;
 
-        for _ in 0..self.retries {
+        for i in 0..self.retries {
+            log!(
+                trace,
+                "Sending HTTP POST request to {:?}, attempt: {:?}",
+                url,
+                i + 1
+            );
             let mut request = http::Request::post(url, vec![request_body.clone()])
                 .add_header("content-type", "application/json");
 
@@ -641,17 +698,38 @@ impl<'a> DdcClient<'a> {
         }
 
         if let Some(e) = error {
+            log!(
+                error,
+                "❌ HTTP POST client error for url {:?}, error {:?}",
+                url,
+                e
+            );
             return Err(e);
         }
 
         let response = match maybe_response {
             Some(r) => r,
-            None => return Err(http::Error::Unknown),
+            None => {
+                log!(
+                    error,
+                    "❌ HTTP POST client error for url {:?}, no response",
+                    url
+                );
+                return Err(http::Error::Unknown);
+            }
         };
 
         if response.code >= 500 {
+            log!(
+                error,
+                "❌ HTTP POST client error for url {:?}, status code is {:?}",
+                url,
+                response.code
+            );
             return Err(http::Error::Unknown);
         }
+
+        log!(trace, "HTTP POST request to {:?} completed", url,);
 
         Ok(response)
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+pub const LOG_TARGET: &str = "ddc-api";
+
 pub mod api;
 pub mod client;
 pub mod verification;
