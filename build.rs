904f6c97c0ddbd51271c8645f3a32eef23cfc9bb
jaxter03 <krishna.singh9926@gmail.com>
2025-10-15 15:43:07 +0530

    Updated DS

--- build.rs
+++ build.rs
@@ -3,6 +3,10 @@ use std::io::Result;
 fn main() -> Result<()> {
     let mut prost_build = prost_build::Config::new();
     prost_build.protoc_arg("--experimental_allow_proto3_optional");
+    
+    // Configure for no_std environment
+    prost_build.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
+    
     prost_build.compile_protos(
         &[
             "src/protos/signature.proto",
